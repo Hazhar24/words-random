34c005fe3257f6e4503a479915d073d2897b0ac4
hazhar24 <hazhar24ghaderi@gmail.com>
2023-03-07 22:51:05 +0330

    add border for Choose word

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,4 @@
-import words from 'random-words';
+import words from "random-words";
 import React from "react";
 
 var dataOne = words({ exactly: 20, maxLength: 2 });
@@ -6,9 +6,15 @@ var dataTwo = words({ exactly: 20, maxLength: 4 });
 var dataThree = words({ exactly: 20, maxLength: 7 });
 
 const Tabs = () => {
-  
+  const refreshPage = () => {
+    window.location.reload(true);
+  };
   const [openTab, setOpenTab] = React.useState(1);
-  const refreshPage = () => {window.location.reload(true);}
+  const [Choosewords, setSelection] = React.useState("");
+
+  const selection1 = () => {
+    setSelection(Choosewords + selection1)
+  };
 
   return (
     <>
@@ -16,9 +22,8 @@ const Tabs = () => {
         <div className="w-full">
           <ul className="flex mb-0 list-none flex-wrap pt-3 pb-4 flex-row" role="tablist">
             <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
-              <a
-                className={"text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block bg-blue-200 leading-normal" + (openTab === 1 ? "text-black bg-" + "-600 bg-blue-600 text-white" : "text-" + "-600")}
-                onClick={e => { e.preventDefault(); setOpenTab(1); }}
+              <a className={"text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block bg-blue-100 leading-normal" + (openTab === 1 ? "text-black bg-" + "-600 bg-blue-400 text-white" : "text-" + "-600")}
+                onClick={(e) => {e.preventDefault(); setOpenTab(1);}}
                 data-toggle="tab"
                 href="#link1"
                 role="tablist">
@@ -26,9 +31,8 @@ const Tabs = () => {
               </a>
             </li>
             <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
-              <a
-                className={"text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block bg-blue-200 leading-normal" + (openTab === 2 ? "text-black bg" + "-600 bg-blue-600 text-white" : "text-" + "-600")}
-                onClick={e => { e.preventDefault(); setOpenTab(2); }}
+              <a className={"text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block bg-blue-100 leading-normal" +(openTab === 2 ? "text-black bg" + "-600 bg-blue-400 text-white" : "text-" + "-600")}
+                onClick={(e) => {e.preventDefault();setOpenTab(2);}}
                 data-toggle="tab"
                 href="#link2"
                 role="tablist">
@@ -36,9 +40,8 @@ const Tabs = () => {
               </a>
             </li>
             <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
-              <a
-                className={"text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block bg-blue-200 leading-normal" + (openTab === 3 ? "text-black" + "-600 bg-blue-600 text-white" : "text-" + "-600")}
-                onClick={e => { e.preventDefault(); setOpenTab(3); }}
+              <a className={"text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block bg-blue-100 leading-normal" +(openTab === 3 ? "text-black" + "-600 bg-blue-400 text-white" : "text-" + "-600")}
+               onClick={(e) => {e.preventDefault();setOpenTab(3);}}
                 data-toggle="tab"
                 href="#link3"
                 role="tablist">
@@ -49,43 +52,48 @@ const Tabs = () => {
           <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
             <div className="px-4 py-5 flex-auto">
               <div className="tab-content tab-space">
-
-
                 <div className={openTab === 1 ? "block" : "hidden"} id="link1">
-                  <div className='flex flex-wrap justify-center items-center mx-auto'>
+                  <div className="flex flex-wrap justify-center items-center mx-auto">
                     {dataOne.map((item) => (
-                      <button className='border border-blue-300 font-semibold w-24 h-12 flex items-center justify-center m-1 hover:text-lg'>
+                      <button
+                        className={"border border-blue-300 font-semibold w-24 h-12 flex items-center justify-center m-1 hover:text-lg"}
+                        onClick={selection1}>
                         {item}
                       </button>
                     ))}
                   </div>
                 </div>
                 <div className={openTab === 2 ? "block" : "hidden"} id="link2">
-                  <div className='flex flex-wrap justify-center items-center mx-auto'>
+                  <div className="flex flex-wrap justify-center items-center mx-auto">
                     {dataTwo.map((item) => (
-                      <button className='border border-blue-300 font-semibold w-24 h-12 flex items-center justify-center m-1 hover:text-lg'>
+                      <button className="border border-blue-300 font-semibold w-24 h-12 flex items-center justify-center m-1 hover:text-lg">
                         {item}
                       </button>
                     ))}
                   </div>
                 </div>
                 <div className={openTab === 3 ? "block" : "hidden"} id="link3">
-                  <div className='flex flex-wrap justify-center items-center mx-auto'>
+                  <div className="flex flex-wrap justify-center items-center mx-auto">
                     {dataThree.map((item) => (
-                      <button className='border border-blue-300 font-semibold w-24 h-12 flex items-center justify-center m-1 hover:text-lg'>
+                      <button className="border border-blue-300 font-semibold w-24 h-12 flex items-center justify-center m-1 hover:text-lg">
                         {item}
                       </button>
                     ))}
                   </div>
                 </div>
-
-                
               </div>
             </div>
           </div>
           <div>
-            <button onClick={refreshPage} className='border w-28 h-14 border-green-600 bg-green-300'>Refresh</button>
+            <button
+              onClick={refreshPage}
+              className="border mt-10 w-28 h-14 border-green-400 bg-green-200">
+              Refresh
+            </button>
           </div>
+          <h1 className={"font-bold flex items-center justify-center w-full h-44 mt-20 uppercase px-5 py-3 shadow-lg rounded bg-blue-200 leading-normal"}>
+            {Choosewords}
+          </h1>
         </div>
       </div>
     </>
